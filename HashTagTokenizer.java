b751c8347864c0a31a5e90986fcfa2cfbb392b8a
unknown <barbarbara.aberzon@post.runi.ac.il>
2024-01-29 18:42:06 +0200

    i love code

--- HashTagTokenizer.java
+++ HashTagTokenizer.java
@@ -13,28 +13,41 @@ public class HashTagTokenizer {
 		String[] dictionary = new String[3000];
 
 		In in = new In(fileName);
-
-		// Your code here
-
+		for (int i = 0 ; i < 3000 ; i ++){
+			dictionary[i] = in.readLine();
+		}
+	
 		return dictionary;
 	}
 
 	public static boolean existInDictionary(String word, String []dictionary) {
-		// Your code here
+	 
+	 for (int i = 0 ; i < dictionary.length ; i++ ){
+		if (word.equals(dictionary[i]))
+		return true;
+	 }
+	 return false ;
 	}
 
 	public static void breakHashTag(String hashtag, String[] dictionary) {
+		hashtag = hashtag.toLowerCase();
 
 		// Base case: do nothing (return) if hashtag is an empty string.
         if (hashtag.isEmpty()) {
             return;
         }
- 
+
         int N = hashtag.length();
 
         for (int i = 1; i <= N; i++) {
-		
-        }
+		if  (existInDictionary( hashtag.substring(0 , i) , dictionary)){
+			System.out.println(hashtag.substring(0 , i));
+			hashtag = hashtag.substring(i , N) ; 
+			breakHashTag(hashtag, dictionary); 
+			break ; 
+		 }
+	   }
+	}
+
     }
 
-}
